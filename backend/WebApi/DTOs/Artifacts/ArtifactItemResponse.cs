cb53e07950e9981641d210f5876abfb42bbd85ec
Kurt <kalde.pw@gmail.com>
2026-06-07 02:18:35 +0800

    pipeline issue fix

---
+++ backend/WebApi/DTOs/Artifacts/ArtifactItemResponse.cs
@@ -0,0 +1,7 @@
+namespace WebApi.DTOs.Artifacts;
+
+public class ArtifactItemResponse
+{
+    public string ArtifactType { get; set; } = string.Empty;
+    public string ContentMarkdown { get; set; } = string.Empty;
+}
